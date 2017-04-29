21db98a7ce4839d2a9b50665b9204b7947a6237f
Sebastian <arkan382382@outlook.com>
2017-04-29 14:44:42 +0200

    add method

--- src/testClass.java
+++ src/testClass.java
@@ -15,6 +15,16 @@ public class testClass {
         one.change(50,50);
         // 50 50 9 9
         System.out.println("Show location after change: " + one.height + " " + one.width + " " + one.origin.x + " " + one.origin.y);
+
+        int h = new Rectangle().height; // creates new Rectangle object and immediately gets its height. After this - objec has been executed because
+                                        // program never stored the reference anywhere - objec is unreferenced.
+        System.out.println("h: " + h);
+
+        System.out.println(new Rectangle(100,5).getArea());  // 500
+        System.out.println(one.getArea());                          // 2500   -> after upper change x:50, y:50
+
+        // garbage collector - zwalnianie pamięci. In java you don't have to worry abut destroying them.
+        // Java runtime environment deletes object when it determines that they are no logner being used. This process is called garbage collection.
     }
 }
 
@@ -39,6 +49,10 @@ class Rectangle{
     public Rectangle(Point p){
         origin = p;
     }
+    public Rectangle(int a, int b){
+        width = a;
+        height = b;
+    }
     public Rectangle(Point p, int w, int h){
         origin = new Point(0,0);
         width = w;
@@ -52,5 +66,8 @@ class Rectangle{
     public void change(int x, int y){
         width = x;
         height = y;
+    }
+    public int getArea(){
+        return width * height;
     }
 }
